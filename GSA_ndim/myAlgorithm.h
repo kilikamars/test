88d9c7e0c0ba36b7341c4fa41b9b3622675a8883
kilikamars <zeiger@hotmail.fr>
2018-12-18 17:33:25 +0100

    Update myAlgorithm.h

--- GSA_ndim/myAlgorithm.h
+++ GSA_ndim/myAlgorithm.h
@@ -76,9 +76,9 @@ class Solution
   	//ici vous devez mettre quelques paramètres tels que :
   	
 	private:
-		unsigned int   _independent_runs;         //number of independent runs
-		unsigned int   _nb_evolution_steps;       // number of iterations per run
-		unsigned int   _population_size;		// number of solutions in the population
+		unsigned int   _independent_runs;       //number of independent runs
+		unsigned int   _nb_evolution_steps;     // number of iterations per run
+		unsigned int   _population_size;	// number of solutions in the population
 		unsigned int   _solution_size;	        // size of each particle
 
 	public:
